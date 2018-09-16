b613c84b56b2261adc38aadd9e9f4717ce610acc
sudeen <ranjitkarsudeen14@gmail.com>
2018-09-17 01:52:47 +1000

    user can be created dynamically

--- src/main/java/com/sudin/blog/controllers/UserController.java
+++ src/main/java/com/sudin/blog/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.sudin.blog.controllers;
 
+import com.sudin.blog.entities.Role;
+import com.sudin.blog.entities.User;
 import com.sudin.blog.pojos.UserRegistration;
 import com.sudin.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 public class UserController {
@@ -14,5 +18,13 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "/register")
-    public String register(@RequestBody UserRegistration userRegistration)
+    public String register(@RequestBody UserRegistration userRegistration){
+        if (!userRegistration.getPassword().equals(userRegistration.getPasswordConfirmation()))
+            return "Password do not match";
+        else if (userService.getUser(userRegistration.getUsername())!=null)
+            return "User already exists";
+
+        userService.save(new User(userRegistration.getUsername(),userRegistration.getPassword(), Arrays.asList(new Role("USER"))));
+        return "User Created";
+    }
 }
